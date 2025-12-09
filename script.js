fcb27adf39e53bdf7f7f8efd7f6186f8587e07be
Kelwin Karan <kevkaran@proton.me>
2025-12-09 17:31:55 -0300

    botei os link errado kkkj

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function atualizarRelogio() {
 setInterval(atualizarRelogio, 1000);
 
 atualizarRelogio();
+
+document.querySelector(".retro-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Impede o reload
+    const btn = document.querySelector(".btn-submit");
+    btn.innerHTML = ">> ENVIADO COM SUCESSO <<";
+    btn.style.color = "#000";
+    btn.style.backgroundColor = "#00ff00";
+    setTimeout(() => {
+        document.querySelector(".retro-form").reset();
+        btn.innerHTML = ">> [ENVIAR_DADOS] <<";
+        btn.style.color = "#00ff00";
+        btn.style.backgroundColor = "transparent";
+    }, 3000);
+});
